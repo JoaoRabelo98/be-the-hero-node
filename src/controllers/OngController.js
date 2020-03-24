c55fefe194e245c2b9ab683baab1252c9e41d031
joaorabelo98 <joaorabello99@gmail.com>
2020-03-24 12:06:02 -0300

    first commit

---
+++ src/controllers/OngController.js
@@ -0,0 +1,34 @@
+const crypto = require('crypto');
+const connection = require('../database/connection');
+
+module.exports = {
+  async listAll(request, response) {
+    try {
+      const ongs = await connection('ongs').select('*');
+
+      return response.json(ongs);
+    } catch (err) {
+      return res.status(400).json({message: 'Ops! Houve um erro :s'})
+    }
+  },
+  async create(request, response) {
+    try {
+      const { name, email, whatsapp, city, uf } = request.body;
+
+      const id = crypto.randomBytes(4).toString('HEX');
+
+      await connection('ongs').insert({
+        id,
+        name,
+        email,
+        whatsapp,
+        city,
+        uf
+      });
+
+      return response.json({ id });
+    } catch (err) {
+      return res.status(400).json({ message: 'Ops! Houve um erro ;s' });
+    }
+  },
+}
